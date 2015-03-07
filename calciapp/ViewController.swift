c0f8900b9617ac0748e6786da1b874719c301ab5
Ashish Gaikwad <ash.gkwd@gmail.com>
2015-03-07 16:11:59 +0530

    Chore - Many Misc Improvements and Logic Rewrite

--- calciapp/ViewController.swift
+++ calciapp/ViewController.swift
@@ -10,19 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputLabel: UILabel!
     @IBOutlet weak var display: UILabel!
     var result = 0
-    var input = 0
     var operation = "+"
     
     @IBAction func calculate(sender: AnyObject) {
+        var input = self.inputLabel.text?.toInt()
         switch operation {
         case "+":
-            result = result + input
-        case "+":
-            result = result - input
-        case "+":
-            result = result * input
+            result = result + input!
+        case "-":
+            result = result - input!
+        case "*":
+            result = result * input!
         default:
             result = 0
             input = 0
@@ -34,7 +35,15 @@ class ViewController: UIViewController {
     
     
     @IBAction func updateInput(sender: AnyObject) {
-        input = sender.titleLabel?.text.toInt()
+        self.inputLabel.text = self.inputLabel.text! + sender.currentTitle!!
+    }
+    
+    @IBAction func updateOperation(sender: AnyObject) {
+        operation =
+    }
+    
+    @IBAction func inputClear(sender: AnyObject) {
+        self.inputLabel.text = "0"
     }
     
     override func viewDidLoad() {
